ecce73c0c82684ab0472f9d6c921725828933728
Mohammed Abdulkereem <mohammed-io@outlook.com>
2019-04-11 11:00:29 +0300

    Without JSX

--- src/index.js
+++ src/index.js
@@ -1,12 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = document.getElementById("root");
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const ul = document.createElement("ul");
+root.appendChild(ul);
+
+function addLiToUl(text) {
+  const li = document.createElement("li");
+  const a = document.createElement("a");
+  a.href = "https://google.com";
+  li.appendChild(a);
+
+  const b = document.createElement("b");
+  b.innerHTML = text;
+  a.appendChild(b);
+
+  const divContainer = document.createElement("div");
+  const deleteBtn = document.createElement("button");
+  deleteBtn.innerHTML = "X";
+  deleteBtn.addEventListener("click", () => alert("Delete"));
+
+  const editBtn = document.createElement("button");
+  editBtn.innerHTML = "Edit";
+  editBtn.addEventListener("click", () => alert("Edit"));
+
+  divContainer.append(editBtn, deleteBtn);
+
+  li.appendChild(divContainer);
+
+  ul.appendChild(li);
+}
+
+// addLiToUl('Google')
+// addLiToUl('Facebook')
+// addLiToUl('Gmail')
+// addLiToUl('Twitter')
+
+const div2 = React.createElement(
+  "ul",
+  null,
+  ["Google", "Facebook", "Twitter", "LinkedIn"].map(x => {
+    return React.createElement("li", null, [
+      React.createElement(
+        "a",
+        { href: "https://google.com" },
+        React.createElement("b", null, x)
+      ),
+      React.createElement("div", null, [
+        React.createElement("button", { onClick: () => alert("Edit") }, "Edit"),
+        React.createElement("button", { onClick: () => alert("Delete") }, "X")
+      ])
+    ]);
+  })
+);
+
+ReactDOM.render(div2, root);
